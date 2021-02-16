6bd65a53143e853d17080f21a053651ceb7f9bd8
PeinZero <mahadzx@gmail.com>
2021-02-16 21:17:06 +0500

    Project Somewhat Completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component} from 'react'
 import Auxiliary from './hoc/Auxiliary/Auxiliary'
-import Structure from './components/Structure/Structure'
+import Structure from './hoc/Structure/Structure'
 import BurgerMaker from './containers/BurgerMaker/BurgerMaker'
 
 class App extends Component{
   render(){
     return(
       <Auxiliary>
-        <Structure/>
-        <BurgerMaker/>
+        <Structure>
+          <BurgerMaker/>
+        </Structure>
       </Auxiliary>
     );
   }
